f559c43c309cf1e0d6bde0fa6996b747067fd048
ewtaylor10-alt <ewtaylor10@hotmail.com>
2026-03-22 23:04:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,105 @@
 const songs = [
-{file:"05 Let Down - Remastered.mp3", name:"Let Down"},
-{file:"Britney Spears - Gimme More.mp3", name:"Gimme More"},
-{file:"Dominic Fike - white keys - wurlighost (128k).mp3", name:"White Keys"},
-{file:"RAYE - Escapism. feat. 070 Shake.mp3", name:"Escapism"},
-{file:"SpotiDownloader.com - Champagne Coast - Blood Orange.mp3", name:"Champagne Coast"},
-{file:"Tate McRae - Sports Car.mp3", name:"Sports Car"},
-{file:"Tate McRae - TIT FOR TAT.mp3", name:"Tit For Tat"},
-{file:"The Kid LAROI - NIGHTS LIKE THIS (lyrics).mp3", name:"Nights Like This"}
+    {
+        file: "05 Let Down - Remastered.mp3",
+        name: "Let Down",
+        cover: "https://i.pinimg.com/736x/1c/f8/70/1cf870c7-9b40-48ff-ab7f-68588dce4353.png"
+    },
+    {
+        file: "Dominic Fike - white keys - wurlighost (128k).mp3",
+        name: "White Keys",
+        cover: "https://i.pinimg.com/736x/7b/f5/41/7bf54150f1d216e9f64de42e3150a76a.jpg"
+    },
+    {
+        file: "Britney Spears - Gimme More.mp3",
+        name: "Gimme More",
+        cover: "https://i.pinimg.com/736x/ea/5b/25/ea5b25f6-614c-4cfe-8b97-1a0f3a402d60.png"
+    },
+    {
+        file: "RAYE - Escapism. feat. 070 Shake.mp3",
+        name: "Escapism",
+        cover: "https://i.pinimg.com/736x/ea/5b/25/ea5b25f6-614c-4cfe-8b97-1a0f3a402d60.png"
+    },
+    {
+        file: "SpotiDownloader.com - Champa.mp3",
+        name: "Champagne Coast",
+        cover: "https://i.pinimg.com/736x/7b/f5/41/7bf54150f1d216e9f64de42e3150a76a.jpg"
+    },
+    {
+        file: "Tate McRae - Sports Car.mp3",
+        name: "Sports Car",
+        cover: "https://i.pinimg.com/736x/1c/f8/70/1cf870c7-9b40-48ff-ab7f-68588dce4353.png"
+    },
+    {
+        file: "Tate McRae - TIT FOR TAT.mp3",
+        name: "Tit For Tat",
+        cover: "https://i.pinimg.com/736x/1c/f8/70/1cf870c7-9b40-48ff-ab7f-68588dce4353.png"
+    },
+    {
+        file: "The Kid LAROI - NIGHTS LIKE THIS (lyrics).mp3",
+        name: "Nights Like This",
+        cover: "https://i.pinimg.com/736x/ea/5b/25/ea5b25f6-614c-4cfe-8b97-1a0f3a402d60.png"
+    }
 ];
 
-let index = Math.floor(Math.random()*songs.length);
-let shuffle = true;
+let currentSong = 0;
 
-const audio = document.getElementById("audio");
-const name = document.getElementById("songName");
-const progress = document.getElementById("progress");
-
+const audio = document.getElementById("bgMusic");
 const playBtn = document.getElementById("playBtn");
-const nextBtn = document.getElementById("nextBtn");
-const prevBtn = document.getElementById("prevBtn");
-const shuffleBtn = document.getElementById("shuffleBtn");
+const songName = document.getElementById("songName");
+const progress = document.getElementById("progress");
+const cover = document.getElementById("cover");
 const volume = document.getElementById("volume");
 
-/* LOAD */
-function loadSong(){
-    audio.src = songs[index].file;
-    name.innerText = songs[index].name;
-}
-
 /* PLAY */
-function playSong(){
+function playSong(index) {
+    audio.src = songs[index].file;
     audio.play();
+
+    songName.innerText = songs[index].name;
+    cover.src = songs[index].cover;
+
     playBtn.innerText = "⏸";
 }
 
-/* PAUSE */
-function pauseSong(){
-    audio.pause();
-    playBtn.innerText = "▶";
+/* NEXT */
+function nextSong() {
+    currentSong = (currentSong + 1) % songs.length;
+    playSong(currentSong);
 }
 
-/* CONTROLS */
-playBtn.onclick = ()=> audio.paused ? playSong() : pauseSong();
+/* PREV */
+function prevSong() {
+    currentSong = (currentSong - 1 + songs.length) % songs.length;
+    playSong(currentSong);
+}
 
-nextBtn.onclick = ()=>{
-    index = shuffle
-        ? Math.floor(Math.random()*songs.length)
-        : (index+1)%songs.length;
-    loadSong();
-    playSong();
-};
-
-prevBtn.onclick = ()=>{
-    index = (index-1+songs.length)%songs.length;
-    loadSong();
-    playSong();
-};
-
-shuffleBtn.onclick = ()=>{
-    shuffle = !shuffle;
-    shuffleBtn.style.opacity = shuffle ? "1" : "0.4";
-};
+/* TOGGLE */
+function toggleMusic() {
+    if (audio.paused) {
+        playSong(currentSong);
+    } else {
+        audio.pause();
+        playBtn.innerText = "▶";
+    }
+}
 
 /* PROGRESS */
-audio.ontimeupdate = ()=>{
-    if(audio.duration){
-        progress.style.width = (audio.currentTime/audio.duration)*100 + "%";
+audio.addEventListener("timeupdate", () => {
+    if (audio.duration) {
+        const percent = (audio.currentTime / audio.duration) * 100;
+        progress.style.width = percent + "%";
     }
-};
-
-document.querySelector(".progress-bar").onclick = (e)=>{
-    audio.currentTime =
-        (e.offsetX / e.currentTarget.clientWidth) * audio.duration;
-};
-
-/* VOLUME */
-volume.oninput = ()=> audio.volume = volume.value;
+});
 
 /* AUTO NEXT */
-audio.onended = ()=> nextBtn.click();
+audio.addEventListener("ended", nextSong);
 
-/* LOADER */
-window.onload = ()=>{
-    loadSong();
-    setTimeout(()=> document.getElementById("loader").style.display="none",800);
-};
+/* VOLUME */
+volume.addEventListener("input", () => {
+    audio.volume = volume.value;
+});
 
 /* AUTOPLAY FIX */
-document.body.addEventListener("click", ()=>{
-    if(audio.paused) playSong();
-},{once:true});
-
-/* MOUSE GLOW */
-document.addEventListener("mousemove", e=>{
-    const glow = document.getElementById("cursorGlow");
-    glow.style.left = e.clientX + "px";
-    glow.style.top = e.clientY + "px";
-});
+document.addEventListener("click", () => {
+    if (audio.paused) playSong(currentSong);
+}, { once: true });
