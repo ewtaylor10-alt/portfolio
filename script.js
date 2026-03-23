8d6f8c30cba2a40ea5038034d5699d235b99d0b8
ewtaylor10-alt <ewtaylor10@hotmail.com>
2026-03-22 21:19:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-script.js
+const songs = [
+    "Dominic Fike - white keys - wurlighost (128k).mp3",
+    "05 Let Down - Remastered.mp3",
+    "The Kid LAROI - NIGHTS LIKE THIS (lyrics).mp3",
+    "Tate McRae - Sports Car.mp3",
+    "Tate McRae - TIT FOR TAT.mp3",
+    "RAYE - Escapism. feat. 070 Shake.mp3",
+    "Britney Spears - Gimme More.mp3"
+];
+
+let currentSong = 0;
+const audio = document.getElementById("bgMusic");
+
+function playSong(index) {
+    audio.src = songs[index];
+    audio.play();
+}
+
+function nextSong() {
+    currentSong++;
+    if (currentSong >= songs.length) {
+        currentSong = 0;
+    }
+    playSong(currentSong);
+}
+
+audio.addEventListener("ended", nextSong);
+
+function toggleMusic() {
+    if (audio.paused) {
+        playSong(currentSong);
+    } else {
+        audio.pause();
+    }
+}
