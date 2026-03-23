f227057b960b2b51cead5c984031cf38d5cd79ea
ewtaylor10-alt <ewtaylor10@hotmail.com>
2026-03-22 23:14:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
 const songs = [
-    {
-        file: "05 Let Down - Remastered.mp3",
-        name: "Let Down",
-        cover: "https://i.pinimg.com/736x/1c/f8/70/1cf870c7-9b40-48ff-ab7f-68588dce4353.png"
-    },
-    {
-        file: "Dominic Fike - white keys - wurlighost (128k).mp3",
-        name: "White Keys",
-        cover: "https://i.pinimg.com/736x/7b/f5/41/7bf54150f1d216e9f64de42e3150a76a.jpg"
-    },
-    {
-        file: "Britney Spears - Gimme More.mp3",
-        name: "Gimme More",
-        cover: "https://i.pinimg.com/736x/ea/5b/25/ea5b25f6-614c-4cfe-8b97-1a0f3a402d60.png"
-    },
-    {
-        file: "RAYE - Escapism. feat. 070 Shake.mp3",
-        name: "Escapism",
-        cover: "https://i.pinimg.com/736x/ea/5b/25/ea5b25f6-614c-4cfe-8b97-1a0f3a402d60.png"
-    },
-    {
-        file: "SpotiDownloader.com - Champa.mp3",
-        name: "Champagne Coast",
-        cover: "https://i.pinimg.com/736x/7b/f5/41/7bf54150f1d216e9f64de42e3150a76a.jpg"
-    },
-    {
-        file: "Tate McRae - Sports Car.mp3",
-        name: "Sports Car",
-        cover: "https://i.pinimg.com/736x/1c/f8/70/1cf870c7-9b40-48ff-ab7f-68588dce4353.png"
-    },
-    {
-        file: "Tate McRae - TIT FOR TAT.mp3",
-        name: "Tit For Tat",
-        cover: "https://i.pinimg.com/736x/1c/f8/70/1cf870c7-9b40-48ff-ab7f-68588dce4353.png"
-    },
-    {
-        file: "The Kid LAROI - NIGHTS LIKE THIS (lyrics).mp3",
-        name: "Nights Like This",
-        cover: "https://i.pinimg.com/736x/ea/5b/25/ea5b25f6-614c-4cfe-8b97-1a0f3a402d60.png"
-    }
+    { file: "05 Let Down - Remastered.mp3", name: "Let Down" },
+    { file: "Dominic Fike - white keys - wurlighost (128k).mp3", name: "White Keys" },
+    { file: "Britney Spears - Gimme More.mp3", name: "Gimme More" },
+    { file: "RAYE - Escapism. feat. 070 Shake.mp3", name: "Escapism" },
+    { file: "SpotiDownloader.com - Champa.mp3", name: "Champagne Coast" },
+    { file: "Tate McRae - Sports Car.mp3", name: "Sports Car" },
+    { file: "Tate McRae - TIT FOR TAT.mp3", name: "Tit For Tat" },
+    { file: "The Kid LAROI - NIGHTS LIKE THIS (lyrics).mp3", name: "Nights Like This" }
 ];
 
 let currentSong = 0;
@@ -56,50 +24,43 @@ function playSong(index) {
     audio.play();
 
     songName.innerText = songs[index].name;
-    cover.src = songs[index].cover;
+    cover.src = "c0bba8165dbb9982fa17e300d66f8264.jpg";
 
     playBtn.innerText = "⏸";
 }
 
-/* NEXT */
-function nextSong() {
-    currentSong = (currentSong + 1) % songs.length;
-    playSong(currentSong);
+function nextSong(){ currentSong=(currentSong+1)%songs.length; playSong(currentSong); }
+function prevSong(){ currentSong=(currentSong-1+songs.length)%songs.length; playSong(currentSong); }
+
+function toggleMusic(){
+    if(audio.paused) playSong(currentSong);
+    else { audio.pause(); playBtn.innerText="▶"; }
 }
 
-/* PREV */
-function prevSong() {
-    currentSong = (currentSong - 1 + songs.length) % songs.length;
-    playSong(currentSong);
-}
-
-/* TOGGLE */
-function toggleMusic() {
-    if (audio.paused) {
-        playSong(currentSong);
-    } else {
-        audio.pause();
-        playBtn.innerText = "▶";
-    }
-}
-
-/* PROGRESS */
-audio.addEventListener("timeupdate", () => {
-    if (audio.duration) {
-        const percent = (audio.currentTime / audio.duration) * 100;
-        progress.style.width = percent + "%";
+audio.addEventListener("timeupdate",()=>{
+    if(audio.duration){
+        progress.style.width=(audio.currentTime/audio.duration)*100+"%";
     }
 });
 
-/* AUTO NEXT */
-audio.addEventListener("ended", nextSong);
+volume.addEventListener("input",()=> audio.volume=volume.value);
 
-/* VOLUME */
-volume.addEventListener("input", () => {
-    audio.volume = volume.value;
+/* AUTOPLAY */
+document.addEventListener("click",()=>{ if(audio.paused) playSong(currentSong); },{once:true});
+
+/* CURSOR + TRAIL */
+const cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", e => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+
+    const trail = document.createElement("div");
+    trail.className = "trail";
+    trail.style.left = e.clientX + "px";
+    trail.style.top = e.clientY + "px";
+
+    document.body.appendChild(trail);
+
+    setTimeout(()=> trail.remove(), 300);
 });
-
-/* AUTOPLAY FIX */
-document.addEventListener("click", () => {
-    if (audio.paused) playSong(currentSong);
-}, { once: true });
