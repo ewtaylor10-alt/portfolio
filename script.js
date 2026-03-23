b6b4f54c251d0fccb313e55e72305aa8f2019bfa
ewtaylor10-alt <ewtaylor10@hotmail.com>
2026-03-22 21:33:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,33 @@ const songs = [
     "Dominic Fike - white keys - wurlighost (128k).mp3",
     "05 Let Down - Remastered.mp3",
     "The Kid LAROI - NIGHTS LIKE THIS (lyrics).mp3",
-    "Tate McRae - Sports Car.mp3",
-    "Tate McRae - TIT FOR TAT.mp3",
-    "RAYE - Escapism. feat. 070 Shake.mp3",
-    "Britney Spears - Gimme More.mp3"
+    "Tate McRae - Sports Car.mp3"
 ];
 
 let currentSong = 0;
 const audio = document.getElementById("bgMusic");
+const songName = document.getElementById("songName");
+
+// update song name
+function updateName() {
+    songName.innerText = songs[currentSong];
+}
 
 function playSong(index) {
     audio.src = songs[index];
     audio.play();
+    updateName();
 }
 
 function nextSong() {
-    currentSong++;
-    if (currentSong >= songs.length) {
-        currentSong = 0;
-    }
+    currentSong = (currentSong + 1) % songs.length;
     playSong(currentSong);
 }
 
-audio.addEventListener("ended", nextSong);
+function prevSong() {
+    currentSong = (currentSong - 1 + songs.length) % songs.length;
+    playSong(currentSong);
+}
 
 function toggleMusic() {
     if (audio.paused) {
@@ -33,3 +37,12 @@ function toggleMusic() {
         audio.pause();
     }
 }
+
+audio.addEventListener("ended", nextSong);
+
+// 🔥 auto play after first click anywhere
+document.body.addEventListener("click", () => {
+    if (audio.paused) {
+        playSong(currentSong);
+    }
+}, { once: true });
